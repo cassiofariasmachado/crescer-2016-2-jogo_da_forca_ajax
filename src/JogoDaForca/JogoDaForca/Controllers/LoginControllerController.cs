febd1033ff17e619420b1fed0f4691dbbc0c0dbf
Gabriel <rosa.gabriel@cwi.com.br>
2016-11-12 10:34:15 -0200

    feat(Login): implementar login no back-end

---
+++ src/JogoDaForca/JogoDaForca/Controllers/LoginControllerController.cs
@@ -0,0 +1,25 @@
+﻿using JogoDaForca.Servicos;
+using JogoDaForcaDominio;
+using System.Web.Mvc;
+
+namespace JogoDaForca.Controllers
+{
+    public class LoginControllerController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Entrar(string nome, string senha)
+        {
+            UsuarioServico usuarioServico = ServicoDeDependencias.MontarUsuarioServico();
+
+            Usuario usuario = usuarioServico.BuscarPorAutenticacao(nome, senha);
+
+            return null;
+        }
+    }
+}
