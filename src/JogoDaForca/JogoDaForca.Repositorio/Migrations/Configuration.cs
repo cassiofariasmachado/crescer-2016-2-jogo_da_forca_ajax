03f2135be668811a80cf3a1e226f3a4cc9c4cfb6
Cassio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-12 16:36:01 -0200

    feat(Configuration): adicionar seed para popular tabela de palavras com 60 palavras.

--- src/JogoDaForca/JogoDaForca.Repositorio/Migrations/Configuration.cs
+++ src/JogoDaForca/JogoDaForca.Repositorio/Migrations/Configuration.cs
@@ -1,6 +1,9 @@
 namespace JogoDaForca.Repositorio.Migrations
 {
+    using JogoDaForcaDominio;
     using System;
+    using System.Collections;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -26,6 +29,77 @@ namespace JogoDaForca.Repositorio.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+            var palavras = new List<string>{
+                                                "Gancho",
+                                                "Dividir",
+                                                "Erro",
+                                                "Torre Eiffel",
+                                                "Tomar",
+                                                "Motor",
+                                                "Centro",
+                                                "Formigueiro",
+                                                "Dois",
+                                                "Gordura",
+                                                "Lobisomem",
+                                                "Estufa",
+                                                "Tripe",
+                                                "Vazamento",
+                                                "Cachos",
+                                                "Educaçao",
+                                                "Pinturas",
+                                                "Radiaçao",
+                                                "Operadores",
+                                                "Siames",
+                                                "Barril",
+                                                "Para-raios",
+                                                "Bazuca",
+                                                "Executivo",
+                                                "Urgencia",
+                                                "Irrigar",
+                                                "Pontape",
+                                                "Uva",
+                                                "Microscopio",
+                                                "Curva",
+                                                "Narraçao",
+                                                "Cabo",
+                                                "Enterrar",
+                                                "Inscriçao",
+                                                "Enfermeira",
+                                                "Vermelho",
+                                                "Chamada",
+                                                "Camadas",
+                                                "Escuro",
+                                                "Lobisomem",
+                                                "Diagrama",
+                                                "Coisa",
+                                                "Machado",
+                                                "Analise",
+                                                "Valentao",
+                                                "Soldado",
+                                                "Avestruz",
+                                                "Teoria",
+                                                "Calça",
+                                                "Antibiotico",
+                                                "Sapato",
+                                                "Perfurar",
+                                                "Quebra cabeça",
+                                                "Dividir",
+                                                "Permanente",
+                                                "Geometria",
+                                                "Peregrino",
+                                                "Membrana",
+                                                "Misto",
+                                                "Orelha"
+                                        };
+
+            var listaDePalavras = new List<Palavra>();
+
+            foreach (var palavra in palavras)
+            {
+                listaDePalavras.Add(new Palavra() { Vocabulo = palavra });
+            }
+
+            context.Palavra.AddOrUpdate(p => p.Vocabulo, listaDePalavras.ToArray());
         }
     }
 }
