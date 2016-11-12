690b974b9bbb912f72ed2afebe04cff513c3c1ce
Gabriel <rosa.gabriel@cwi.com.br>
2016-11-12 10:09:24 -0200

    feat(Repositorio): implementar  UsuarioRepositorio e PalavraRepositorio

---
+++ src/JogoDaForca/JogoDaForca.Repositorio/UsuarioRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using JogoDaForcaDominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDaForca.Repositorio
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        public Usuario BuscarPorNome(string nome)
+        {
+            using (var context = new ContextoDeDados())
+            {
+                return context.Usuario.FirstOrDefault(u => u.Nome.Equals(nome));
+            }
+        }
+    }
+}
