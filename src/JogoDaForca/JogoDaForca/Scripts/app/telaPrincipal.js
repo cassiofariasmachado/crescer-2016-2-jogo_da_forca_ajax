2cee91c9403ffe4258fbfc73c30ffe659253457c
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-14 18:05:24 -0200

    fix(telaPrincipal): corrigir o "then" da tela

--- src/JogoDaForca/JogoDaForca/Scripts/app/telaPrincipal.js
+++ src/JogoDaForca/JogoDaForca/Scripts/app/telaPrincipal.js
@@ -1,18 +1,106 @@
 ﻿class TelaPrincipal {
-  
+
   constructor(seletor) {
     this.$elem = $(seletor);
-    this.registrarBindsEventos();
-    this.renderizarEstadoInicial();
-    jogoDaForca.iniciarJogo(modo);
+    let self = this;
+    this.renderizarEstadoInicial()
+      .then(() => {
+        self.registrarBindsEventos();
+        self.iniciarJogo('normal')
+      })
   }
 
   registrarBindsEventos() {
     this.$palavra = $('#palavra');
+    this.$tentativas = $('#tentativas');
+    this.$tentativasErradas = $('#tentativas-erradas');
+    this.$letra = $('#letra');
   }
 
   renderizarEstadoInicial() {
     this.$elem.show();
-    jogoDaForca.render('#tela-principal', 'tela-principal');
+    return jogoDaForca.render('#tela-principal', 'tela-principal');
   }
+
+  iniciarJogo(modo) {
+    let palavras = new Palavras();
+    palavras.pegarPalavraAleatoria(modo).then(
+      res => {
+        let palavra = res.vocabulo.toLocaleUpperCase();
+        let palavraCriptografada = jogoDaForca.gerarPalavraCriptografada(palavra.length);
+        let pontuacao = 0;
+        let tentativas = 0;
+        let tentativasErradas = 0;
+
+        this.atualizarPalavra(palavraCriptografada);
+        this.atualizarTentativas(tentativas);
+
+        this.$letra.keydown(function (event) {
+          if (jogoDaForca.ehLetra(event.keyCode)) {
+            let letra = event.key.toLocaleUpperCase();
+
+            if (modo === 'normal') {
+              this.modoNormal();
+            } else if (modo === 'bh') {
+              this.modoBh();
+            }
+
+            if (!palavraCriptografada.contains('-')) {
+              pontuacao++;
+              return 'Rodada completa'
+            }
+
+          }
+        });
+      }
+    );
+  }
+
+  modoNormal() {
+    if (tentativasErradas > 5) {
+      return 'Game over';
+    }
+    if (palavra.contains(letra)) {
+      palavraCriptografada = jogoDaForca.substituirLetra(palavra, palavraCriptografada, letra);
+      this.atualizarPalavra(palavraCriptografada);
+    }
+    else {
+      tentativasErradas++;
+      this.atualizarTentativasErradas(tentativasErradas);
+    }
+    tentativas++;
+    this.atualizarTentativas(tentativas);
+  }
+
+  modoBh() {
+    if (tentativasErradas > 2) {
+      return 'Game over';
+    }
+    if (palavra.contains(letra)) {
+      palavraCriptografada = substituirLetra(palavra, palavraCriptografada, letra);
+      this.atualizarPalavra(palavraCriptografada);
+    }
+    else {
+      tentativasErradas++;
+      this.atualizarTentativasErradas(tentativasErradas);
+    }
+    tentativas++;
+    this.atualizarTentativas(tentativas);
+  }
+
+
+
+
+  atualizarPalavra(novaPalavra) {
+    this.$palavra.text(novaPalavra)
+  }
+
+  atualizarTentativas(tentativas) {
+    this.$tentativas.text(tentativas)
+  }
+
+  atualizarTentativasErradas(tentativasErradas) {
+    this.$tentativasErradas.text(tentativasErradas)
+  }
+
 }
