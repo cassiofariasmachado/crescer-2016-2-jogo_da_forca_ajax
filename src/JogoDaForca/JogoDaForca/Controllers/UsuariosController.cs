e84581b922dfe12f021617d981297f778e65d35c
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-14 11:33:07 -0200

    feat(UsuariosController): adicionar controller para usuario com métodos GetUsuario() e PostUsuario().

---
+++ src/JogoDaForca/JogoDaForca/Controllers/UsuariosController.cs
@@ -0,0 +1,44 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Http.Description;
+using JogoDaForca.Dominio;
+using JogoDaForca.Dominio.Interfaces;
+using JogoDaForca.Servicos;
+
+namespace JogoDaForca.Controllers
+{
+    public class UsuariosController : ApiController
+    {
+        private IUsuarioRepositorio usuarioRepositorio = ServicoDeDependencias.MontarUsuarioRepositorio();
+
+        // GET: api/Usuarios/5
+        [ResponseType(typeof(Usuario))]
+        public IHttpActionResult GetUsuario(int id)
+        {
+            Usuario usuario = usuarioRepositorio.BuscarPorId(id);
+            if (usuario == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(usuario);
+        }
+
+        // POST: api/Usuarios
+        [ResponseType(typeof(Usuario))]
+        public IHttpActionResult PostUsuario(Usuario usuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            usuarioRepositorio.Adicionar(usuario);
+
+            return CreatedAtRoute("DefaultApi", new { id = usuario.Id }, usuario);
+        }
+    }
+}
