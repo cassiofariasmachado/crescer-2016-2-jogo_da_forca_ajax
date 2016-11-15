c8aee2ca41679c2f4c50134f5109a3fc5e54062a
Unknown <gabrielfr97@gmail.com>
2016-11-15 17:05:24 -0200

    feat(game over): implementando parte da tela

---
+++ src/JogoDaForca/JogoDaForca/Scripts/app/telaGameOver.js
@@ -0,0 +1,18 @@
+﻿class TelaGameOver {
+
+    constructor(seletor) {
+        this.$elem = $(seletor);
+        this.registrarBindsEventos();
+        this.renderizarEstadoInicial();
+    }
+
+    registrarBindsEventos() {
+        this.$btnVoltarAoJogo = $('#voltar-ao-jogo');
+        this.$btnIrAoLeaderboard = $('#ir-ao-leaderboard');
+
+    }
+
+    renderizarEstadoInicial() {
+        this.$elem.show();
+    }
+}
