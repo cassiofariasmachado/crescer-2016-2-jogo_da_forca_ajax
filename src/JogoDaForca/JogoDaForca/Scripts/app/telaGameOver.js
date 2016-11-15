912e8e7d5d4ec587118ebdd0c6533c5bbf5a21c2
Unknown <gabrielfr97@gmail.com>
2016-11-15 19:53:37 -0200

    feat(Jogo): implementar ç

--- src/JogoDaForca/JogoDaForca/Scripts/app/telaGameOver.js
+++ src/JogoDaForca/JogoDaForca/Scripts/app/telaGameOver.js
@@ -3,13 +3,14 @@
     constructor(seletor) {
         this.$elem = $(seletor);
         this.registrarBindsEventos();
-        this.renderizarEstadoInicial();
+        this.renderizarEstadoInicial().then(() => {
+            this.registrarBindsEventos();
+        });
     }
 
     registrarBindsEventos() {
         this.$btnVoltarAoJogo = $('#voltar-ao-jogo');
         this.$btnIrAoLeaderboard = $('#ir-ao-leaderboard');
-
     }
 
     renderizarEstadoInicial() {
--- src/JogoDaForca/JogoDaForca/Scripts/app/telaPrincipal.js
+++ src/JogoDaForca/JogoDaForca/Scripts/app/telaPrincipal.js
@@ -1,126 +1,131 @@
 ﻿class TelaPrincipal {
 
-  constructor(seletor) {
-    this.$elem = $(seletor);
-    let self = this;
-    this.renderizarEstadoInicial()
-      .then(() => {
-        self.registrarBindsEventos();
-        self.iniciarJogo('normal');
-      })    
-  }
-
-  registrarBindsEventos() {
-    this.$palavra = $('#palavra');
-    this.$tentativas = $('#tentativas');
-    this.$tentativasErradas = $('#tentativas-erradas');
-    this.$letra = $('#letra');
-    this.$palpite = $('#palpite');
-    this.$btnPalpitar = $('#btn-palpitar')
-  }
-
-  renderizarEstadoInicial() {
-    this.$elem.show();
-    return jogoDaForca.render('#tela-principal', 'tela-principal');
-  }
-
-  iniciarJogo(modo) {
-    let palavras = new Palavras();
-    palavras.pegarPalavraAleatoria(modo).then(
-      resposta => {
-
-        this.palavra = resposta.vocabulo.toLocaleUpperCase();
-        this.palavraCriptografada = jogoDaForca.gerarPalavraCriptografada(this.palavra.length);
-        this.pontuacao = 0;
-        this.tentativas = 0;
-        this.tentativasErradas = 0;
-
+    constructor(seletor) {
+        this.$elem = $(seletor);
         let self = this;
+        this.renderizarEstadoInicial()
+          .then(() => {
+              self.registrarBindsEventos();
+              self.iniciarJogo('bh');
+          })
+    }
 
-        self.atualizarPalavra(self.palavraCriptografada);
-        self.atualizarTentativas(self.tentativas);
+    registrarBindsEventos() {
+        this.$palavra = $('#palavra');
+        this.$tentativas = $('#tentativas');
+        this.$tentativasErradas = $('#tentativas-erradas');
+        this.$letra = $('#letra');
+        this.$palpite = $('#palpite');
+        this.$btnPalpitar = $('#btn-palpitar')
+    }
 
-        self.$letra.keydown(function (event) {
-          if (jogoDaForca.ehLetra(event.keyCode)) {
-            self.letra = event.key.toLocaleUpperCase();
+    renderizarEstadoInicial() {
+        this.$elem.show();
+        return jogoDaForca.render('#tela-principal', 'tela-principal');
+    }
 
-            switch(modo) {
-              case 'normal':
-                  self.modoNormal();
-                  break;
-              case 'bh':
-                  self.modoBh();
-                  break;
-              default:
-                  cosole.log('Verificar switch do modo de jogo!');
-            }
+    iniciarJogo(modo) {
+        let palavras = new Palavras();
+        palavras.pegarPalavraAleatoria(modo).then(
+          resposta => {
 
-            self.verificarLetra();
+              this.palavra = resposta.vocabulo.toLocaleUpperCase();
+              this.palavraCriptografada = jogoDaForca.gerarPalavraCriptografada(this.palavra.length);
+              this.pontuacao = 0;
+              this.tentativas = 0;
+              this.tentativasErradas = 0;
 
-            if (!self.palavraCriptografada.contains('-')) {
-              self.pontuacao++;
-              console.log('TO-DO: Rodada completa'); 
-            }
+              let self = this;
+
+              self.atualizarPalavra(self.palavraCriptografada);
+              self.atualizarTentativas(self.tentativas);
+
+              self.$letra.keydown(function (event) {
+                  if (jogoDaForca.ehLetra(event.keyCode)) {
+                      self.letra = event.key.toLocaleUpperCase();
+
+                      switch (modo) {
+                          case 'normal':
+                              self.modoNormal();
+                              break;
+                          case 'bh':
+                              self.modoBh();
+                              break;
+                          default:
+                              cosole.log('Verificar switch do modo de jogo!');
+                      }
+
+                      self.verificarLetra();
+
+                      if (!self.palavraCriptografada.contains('-')) {
+                          self.pontuacao++;
+                          console.log('TO-DO: Rodada completa');
+                      }
+                  }
+              });
+
+              self.$btnPalpitar.on('click', function () {
+                  self.$btnPalpitar.text('Palpitando...');
+                  self.$btnPalpitar.attr('disabled', true);
+                  self.palpitar();
+              }
+              );
+
           }
-        });
-
-        self.$btnPalpitar.on('click', function() {
-                                                      self.$btnPalpitar.text('Palpitando...');
-                                                      self.$btnPalpitar.attr('disabled', true);
-                                                      self.palpitar();
-                                                  }
         );
-
-      }
-    );
-  }
-
-  modoNormal() {
-    if (this.tentativasErradas === 5) {
-      console.log('TO-DO: Game over');
     }
-  }
 
-  modoBh() {
-    if (this.tentativasErradas === 2) {
-      console.log('TO-DO: Game over');
+    modoNormal() {
+        if (this.tentativasErradas === 5) {
+            self.telaPrincipal.gameOver().then();
+            console.log('TO-DO: Game over');
+        }
     }
-  }
 
-  verificarLetra() {
-    if (this.palavra.contains(this.letra)) {
-      this.palavraCriptografada = jogoDaForca.substituirLetra(this.palavra, this.palavraCriptografada, this.letra);
-      this.atualizarPalavra(this.palavraCriptografada);
+    modoBh() {
+        if (this.tentativasErradas === 2) {
+            console.log('TO-DO: Game over');
+        }
     }
-    else {
-        this.tentativasErradas++;
-        this.atualizarTentativasErradas(this.tentativasErradas);
+
+    verificarLetra() {
+        if (this.palavra.contains(this.letra)) {
+            this.palavraCriptografada = jogoDaForca.substituirLetra(this.palavra, this.palavraCriptografada, this.letra);
+            this.atualizarPalavra(this.palavraCriptografada);
+        }
+        else {
+            this.tentativasErradas++;
+            this.atualizarTentativasErradas(this.tentativasErradas);
+        }
+        this.tentativas++;
+        this.atualizarTentativas(this.tentativas);
     }
-    this.tentativas++;
-    this.atualizarTentativas(this.tentativas);
-  }
 
-  atualizarPalavra(novaPalavra) {
-    this.$palavra.text(novaPalavra)
-  }
-
-  atualizarTentativas(tentativas) {
-    this.$tentativas.text(tentativas)
-  }
-
-  atualizarTentativasErradas(tentativasErradas) {
-    this.$tentativasErradas.text(tentativasErradas)
-  }
-
-  palpitar() {
-    let palavraPalpitada = this.$palpite.val().toLocaleUpperCase();
-    if(this.palavra === palavraPalpitada) {
-      self.pontuacao += 2;
-      console.log('TO-DO: Rodada completa'); 
+    atualizarPalavra(novaPalavra) {
+        this.$palavra.text(novaPalavra)
     }
-    else{
-      console.log('TO-DO: Game over');
+
+    atualizarTentativas(tentativas) {
+        this.$tentativas.text(tentativas)
     }
-  }
+
+    atualizarTentativasErradas(tentativasErradas) {
+        this.$tentativasErradas.text(tentativasErradas)
+    }
+
+    palpitar() {
+        let palavraPalpitada = this.$palpite.val().toLocaleUpperCase();
+        if (this.palavra === palavraPalpitada) {
+            self.pontuacao += 2;
+            console.log('TO-DO: Rodada completa');
+        }
+        else {
+            console.log('TO-DO: Game over');
+        }
+    }
+
+    gameOver() {
+        return jogoDaForca.render('#game-over', 'game-over');
+    }
 
 }
