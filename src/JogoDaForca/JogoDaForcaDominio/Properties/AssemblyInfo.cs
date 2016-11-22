00afe704ea45fab45fdc1e63c08cf2016b1111ce
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-21 23:54:48 -0200

    fix(JogoDaForca.Dominio, AssemblyInfo): corrigir nome do projeto no assembly.

--- src/JogoDaForca/JogoDaForcaDominio/Properties/AssemblyInfo.cs
+++ src/JogoDaForca/JogoDaForcaDominio/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JogoDaForcaDominio")]
+[assembly: AssemblyTitle("JogoDaForca.Dominio")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JogoDaForcaDominio")]
+[assembly: AssemblyProduct("JogoDaForca.Dominio")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
