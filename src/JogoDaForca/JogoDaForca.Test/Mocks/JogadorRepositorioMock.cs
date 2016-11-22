0c72c147f774975b9d3e82c8083bf71bd73508d7
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-22 00:05:58 -0200

    feat(JogoDaForca.Test): adicionar projeto de testes a Solution.

---
+++ src/JogoDaForca/JogoDaForca.Test/Mocks/JogadorRepositorioMock.cs
@@ -0,0 +1,38 @@
+﻿using JogoDaForca.Dominio;
+using JogoDaForca.Dominio.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JogoDaForca.Test.Mocks
+{
+    public class JogadorRepositorioMock : IJogadorRepositorio
+    {
+        private IList<Jogador> jogadores;
+
+        public JogadorRepositorioMock()
+        {
+            this.jogadores = new List<Jogador>();
+            jogadores.Add(new Jogador(1, "Cassio"));
+            jogadores.Add(new Jogador(2, "Joao"));
+            jogadores.Add(new Jogador(3, "Bibiana"));
+            jogadores.Add(new Jogador(4, "Paula"));
+        }
+
+        public Jogador Buscar(string nome)
+        {
+            return this.jogadores.SingleOrDefault(_ => _.Nome.Equals(nome));
+        }
+
+        public Jogador Buscar(int id)
+        {
+            return this.jogadores.SingleOrDefault(_ => _.Id == id);
+        }
+
+        public void Inserir(Jogador jogador)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/JogoDaForca/JogoDaForca.Test/Mocks/PalavraRepositorioMock.cs
@@ -0,0 +1,43 @@
+﻿using JogoDaForca.Dominio;
+using JogoDaForca.Dominio.Repositorios;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDaForca.Test.Mocks
+{
+    public class PalavraRepositorioMock : IPalavraRepositorio
+    {
+        private IList<Palavra> palavras;   
+
+        public PalavraRepositorioMock()
+        {
+            this.palavras = new List<Palavra>();
+            palavras.Add(new Palavra(1, "Caderno"));
+            palavras.Add(new Palavra(2, "Pederneira"));
+            palavras.Add(new Palavra(3, "Pedra"));
+            palavras.Add(new Palavra(4, "Cadarço"));
+            palavras.Add(new Palavra(5, "Abacaxi"));
+            palavras.Add(new Palavra(6, "Vassoura"));
+        }
+
+        public Palavra Buscar(int id)
+        {
+            return this.palavras.Single(_ => _.Id == id);
+        }
+
+        public IEnumerable<Palavra> BuscarPalavrasAleatorias(int qtdCaracteresMinima = 0)
+        {
+            if (qtdCaracteresMinima != 0)
+            {
+                return this.palavras.Where(_ => _.Texto.Length > qtdCaracteresMinima)
+                                    .OrderBy(o => Guid.NewGuid());
+            }
+
+            return this.palavras.OrderBy(o => Guid.NewGuid());
+        }
+    }
+}
