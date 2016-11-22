d1ba2cf3e3e142bfa829567d5533987ecf9935bb
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-21 23:41:18 -0200

    refactor(Leaderboard): renomear a classe Jogada para Leaderboard modificando a propriedade dificuldade para uma Enum.

---
+++ src/JogoDaForca/JogoDaForcaDominio/Leaderboard.cs
@@ -0,0 +1,20 @@
+﻿namespace JogoDaForca.Dominio
+{
+    public class Leaderboard
+    {
+        public int Id { get; set; }
+        public Jogador Jogador { get; set; }
+        public int Pontuacao { get; set; }
+        public Dificuldade Dificuldade { get; set; }
+
+        public Leaderboard() { }
+
+        public Leaderboard(int id, Jogador jogador, int pontuacao, Dificuldade dificuldade)
+        {
+            this.Id = id;
+            this.Jogador = jogador;
+            this.Pontuacao = pontuacao;
+            this.Dificuldade = dificuldade;
+        }
+    }
+}
