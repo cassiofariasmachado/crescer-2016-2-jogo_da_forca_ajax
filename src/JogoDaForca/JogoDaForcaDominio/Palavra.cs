2158824a41e8a237ba59dc84adf3bb72006b0db7
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-21 23:44:01 -0200

    refactor(Palavra): renomear propriedade Vocabulo para Texto.

--- src/JogoDaForca/JogoDaForcaDominio/Palavra.cs
+++ src/JogoDaForca/JogoDaForcaDominio/Palavra.cs
@@ -1,14 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JogoDaForca.Dominio
+﻿namespace JogoDaForca.Dominio
 {
     public class Palavra
     {
         public int Id { get; set; }
-        public string Vocabulo { get; set; }
+        public string Texto { get; set; }
+
+        public Palavra() { }
+
+        public Palavra(int id, string texto)
+        {
+            this.Id = id;
+            this.Texto = texto;
+        }
     }
 }
