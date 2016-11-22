1730ac686087042d59f301dc82cfeda802d0f195
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-21 23:43:06 -0200

    refactor(Jogador): renomear classe Usuario para Jogador.

---
+++ src/JogoDaForca/JogoDaForcaDominio/Jogador.cs
@@ -0,0 +1,16 @@
+﻿namespace JogoDaForca.Dominio
+{
+    public class Jogador
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public Jogador() { }
+
+        public Jogador(int id, string nome)
+        {
+            this.Id = id;
+            this.Nome = nome;
+        }
+    }
+}
