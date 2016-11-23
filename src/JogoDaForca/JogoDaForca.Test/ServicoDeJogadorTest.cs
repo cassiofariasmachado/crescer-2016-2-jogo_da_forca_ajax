caeeddb95cdbae0a955c40945570f44088ed3bd8
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-23 10:20:11 -0200

    test(ServicoDeJogador): adicionar classe de teste para ServicoDeJogador.

---
+++ src/JogoDaForca/JogoDaForca.Test/ServicoDeJogadorTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JogoDaForcaDominio.Servicos;
+using JogoDaForca.Test.Mocks;
+using JogoDaForca.Dominio;
+
+namespace JogoDaForca.Test
+{
+    [TestClass]
+    public class ServicoDeJogadorTest
+    {
+        private ServicoDeJogador servicoDeJogador = new ServicoDeJogador(new JogadorRepositorioMock());
+
+        [TestMethod]
+        public void AutenticarJogadorDeveRetornarUmJogador()
+        {
+            Jogador jogador = servicoDeJogador.AutenticarJogador("Cassio");
+
+            Assert.AreEqual(1, jogador.Id);
+            Assert.AreEqual("Cassio", jogador.Nome);
+        }
+
+        [TestMethod]
+        public void AutenticarJogadorDeveCriarERetornarUmJogador()
+        {
+            Jogador jogador = servicoDeJogador.AutenticarJogador("Paulo");
+
+            Assert.AreEqual(5, jogador.Id);
+            Assert.AreEqual("Paulo", jogador.Nome);
+        }
+    }
+}
