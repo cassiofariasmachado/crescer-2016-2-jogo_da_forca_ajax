a53f6265a3daa5204a5cd33d55eda35caf3f57a8
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-23 10:35:40 -0200

    feat(telaJogo): adicionar telaJogo.js reponsável por  controlar o jogo.

---
+++ src/JogoDaForca/JogoDaForca/Scripts/app/telaJogo.js
@@ -0,0 +1,69 @@
+﻿class TelaJogo {
+
+    constructor(seletor) {
+        this.$elem = $(seletor);
+        this.renderizarEstadoInicial().then(resposta => {
+            this.registrarBindsEventos();
+            this.novaRodada();
+        });
+
+    }
+
+    registrarBindsEventos() {
+        this.$body = $('body');
+        this.$palavra = $('#palavra');
+        this.dificuldade = localStorage.getItem('dificuldade');
+        this.palavras = new Palavras();
+        this.registraEventoTeclado();
+    }
+
+    renderizarEstadoInicial() {
+        this.$elem.show();
+        return jogoDaForca.render('#tela-jogo', 'tela-jogo');
+    }
+
+    novaRodada() {
+        this.palavras.buscarPalavraAleatoria(this.dificuldade).then(resposta => {
+            this.palavra = resposta;
+            this.palavraCriptografada = jogoDaForca.gerarPalavraCriptografada(this.palavra.texto);
+            this.pontuacao = 0;
+
+            this.atualizarPalavra();
+        });
+
+
+    }
+
+    registraEventoTeclado() {
+        this.$body.keydown(evento => {
+            if (jogoDaForca.ehLetra(evento.keyCode)) {
+                let letra = evento.key;
+                this.substituirLetra(letra);
+                this.verificaEncerramentoDaRodada();
+                this.atualizarPalavra();
+            }
+        });
+    }
+
+
+    substituirLetra(letra) {
+        let palavra = this.palavra.texto;
+        for (let i = 0, len = palavra.length; i < len; i++) {
+            if (palavra[i] === letra) {
+                this.palavraCriptografada = this.palavraCriptografada.replaceAt(i, letra);
+            }
+        }
+    }
+
+    atualizarPalavra() {
+        this.$palavra.text(this.palavraCriptografada);
+    }
+
+    verificaEncerramentoDaRodada() {
+        if (!this.palavraCriptografada.contains('-')) {
+            this.pontuacao++;
+            this.novaRodada();
+            console.log('Encerrou a jogada!!!')
+        }
+    }
+}
