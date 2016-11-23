627871abfab981ef8a8caa286e67d920c81d0c4a
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-23 10:31:20 -0200

    feat(storageExtents): adicionar storaExtents.js como métodos getObject() e setObject() para armazenar objetos javascript no localStorage.

---
+++ src/JogoDaForca/JogoDaForca/Scripts/app/extents/storageExtents.js
@@ -0,0 +1,7 @@
+﻿Storage.prototype.setObject = function (chave, objeto) {
+    this.setItem(chave, JSON.stringify(objeto))
+}
+
+Storage.prototype.getObject = function (chave) {
+    return JSON.parse(this.getItem(chave));
+}
