dcdf6d8ce9f8c09d07b55979a6d5359359127bed
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-23 10:15:56 -0200

    feat(ServicoDeJogador, ServicoDePalavra): adicionar serviço de jogador com método AutenticarJogador() e serviço de palavra com BuscarPalavraAleatoria().

---
+++ src/JogoDaForca/JogoDaForcaDominio/Servicos/ServicoDeJogador.cs
@@ -0,0 +1,33 @@
+﻿using JogoDaForca.Dominio;
+using JogoDaForca.Dominio.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDaForcaDominio.Servicos
+{
+    public class ServicoDeJogador
+    {
+        private IJogadorRepositorio repositorioJogador;
+
+        public ServicoDeJogador(IJogadorRepositorio repositorioJogador)
+        {
+            this.repositorioJogador = repositorioJogador;
+        }
+
+        public Jogador AutenticarJogador(string nome)
+        {
+            Jogador jogador = this.repositorioJogador.Buscar(nome);
+
+            if(jogador == null)
+            {
+                this.repositorioJogador.Inserir(new Jogador(nome));
+                jogador = this.repositorioJogador.Buscar(nome);
+            }
+
+            return jogador;
+        }
+    }
+}
---
+++ src/JogoDaForca/JogoDaForcaDominio/Servicos/ServicoDePalavra.cs
@@ -0,0 +1,37 @@
+﻿using JogoDaForca.Dominio;
+using JogoDaForca.Dominio.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDaForcaDominio.Servicos
+{
+    public class ServicoDePalavra
+    {
+        private IPalavraRepositorio palavraRepositorio;
+
+        public ServicoDePalavra(IPalavraRepositorio palavraRepositorio)
+        {
+            this.palavraRepositorio = palavraRepositorio;
+        }
+
+        public Palavra BuscarPalavraAleatoria(Dificuldade dificuldade)
+        {
+            Palavra palavras = null;
+            switch (dificuldade)
+            {
+                case Dificuldade.Normal:
+                    palavras = palavraRepositorio.BuscarPalavraAleatoria();
+                    break;
+                case Dificuldade.BH:
+                    palavras = palavraRepositorio.BuscarPalavraAleatoria(12);
+                    break;
+                default:
+                    break;
+            }
+            return palavras;
+        }
+    }
+}
