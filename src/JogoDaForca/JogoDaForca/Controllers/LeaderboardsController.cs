2c3007a64c1255f15480ae1258fb8d25eb2a0e1b
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2016-11-23 11:00:33 -0200

    doc(LeaderboardController): comentar classe para refatorar depois.

--- src/JogoDaForca/JogoDaForca/Controllers/LeaderboardsController.cs
+++ src/JogoDaForca/JogoDaForca/Controllers/LeaderboardsController.cs
@@ -8,60 +8,60 @@ namespace JogoDaForca.Controllers
 {
     public class LeaderboardsController : ApiController
     {
-        //TO-DO: criar serviço para isso
-        private ILeaderboardRepositorio leaderboardRepositorio = ServicoDeDependencias.MontarJogadaRepositorio();
+        ////TO-DO: criar serviço para isso
+        //private ILeaderboardRepositorio leaderboardRepositorio = ServicoDeDependencias.MontarJogadaRepositorio();
 
-        // GET: api/jogadas
-        [ResponseType(typeof(Leaderboard))]
-        public IHttpActionResult GetLeaderboard(int id)
-        {
-            Leaderboard leaderboard = leaderboardRepositorio.Buscar(id);
-            if (leaderboard == null)
-            {
-                return NotFound();
-            }
+        //// GET: api/jogadas
+        //[ResponseType(typeof(Leaderboard))]
+        //public IHttpActionResult GetLeaderboard(int id)
+        //{
+        //    Leaderboard leaderboard = leaderboardRepositorio.Buscar(id);
+        //    if (leaderboard == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return Ok(leaderboard);
-        }
-
-
-        // GET: api/jogadas
-        public IHttpActionResult GetLeaderboard(int pagina = 1, int tamanhoPagina = 5)
-        {
-            var registros = leaderboardRepositorio.Listar(pagina, tamanhoPagina);
-
-            return Ok(new
-            {
-                total = leaderboardRepositorio.ContarRegistros(),
-                dados = registros
-            });
-        }
-
-        // GET: api/jogadas
-        public IHttpActionResult GetLeaderboard(Dificuldade dificuldade, int pagina = 1, int tamanhoPagina = 5)
-        {
-            var registros = leaderboardRepositorio.Listar(pagina, tamanhoPagina, dificuldade);
-
-            return Ok(new
-            {
-                total = leaderboardRepositorio.ContarRegistros(),
-                dados = registros
-            });
-        }
+        //    return Ok(leaderboard);
+        //}
 
 
-        // POST: api/jogadas
-        [ResponseType(typeof(Leaderboard))]
-        public IHttpActionResult PostLeaderboard(Leaderboard leaderboard)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+        //// GET: api/jogadas
+        //public IHttpActionResult GetLeaderboard(int pagina = 1, int tamanhoPagina = 5)
+        //{
+        //    var registros = leaderboardRepositorio.Listar(pagina, tamanhoPagina);
 
-            leaderboardRepositorio.Inserir(leaderboard);
+        //    return Ok(new
+        //    {
+        //        total = leaderboardRepositorio.ContarRegistros(),
+        //        dados = registros
+        //    });
+        //}
 
-            return CreatedAtRoute("DefaultApi", new { id = leaderboard.Id }, leaderboard);
-        }
+        //// GET: api/jogadas
+        //public IHttpActionResult GetLeaderboard(Dificuldade dificuldade, int pagina = 1, int tamanhoPagina = 5)
+        //{
+        //    var registros = leaderboardRepositorio.Listar(pagina, tamanhoPagina, dificuldade);
+
+        //    return Ok(new
+        //    {
+        //        total = leaderboardRepositorio.ContarRegistros(),
+        //        dados = registros
+        //    });
+        //}
+
+
+        //// POST: api/jogadas
+        //[ResponseType(typeof(Leaderboard))]
+        //public IHttpActionResult PostLeaderboard(Leaderboard leaderboard)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    leaderboardRepositorio.Inserir(leaderboard);
+
+        //    return CreatedAtRoute("DefaultApi", new { id = leaderboard.Id }, leaderboard);
+        //}
     }
 }
